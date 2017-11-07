3f74fbe7c5ecd85d0b8cd02d0b008475cef8dc83
benjamin <degroote.benjamin@gmail.com>
2017-11-07 09:33:06 +0100

    init manager class

---
+++ model/manager/Accountmanager.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ */
+class Accountmanager
+{
+
+  private $db; // Instance de PDO
+  public function __construct($db)
+  {
+      $this->setDb($db);
+  }
+  // setter //
+  public function setDb(PDO $db)
+  {
+      $this->db = $db;
+  }
+}
+
+ ?>
