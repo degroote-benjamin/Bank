0714f681ed37914b1b53e8c1ffd4adb281264f06
benjamin <degroote.benjamin@gmail.com>
2017-11-08 10:19:16 +0100

    add bootstrap on all form

--- view/Signup.php
+++ view/Signup.php
@@ -1,14 +1,30 @@
 <?php
 include 'header.php';
  ?>
+ <div class="container d-flex justify-content-center">
+   <div class="row">
 <form class="" action="index.php" method="post">
-<input type="text" name="name" value="">
-<input type="email" name="email" value="">
-<input type="password" name="password" value="">
-<input type="password" name="passwordcheck" value="">
-<input type="submit" name="submitsignup" value="Submit">
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input class="form-control" type="text" name="name" value="" id="name">
+  </div>
+  <div class="form-group">
+    <label for="email">Email</label>
+    <input class="form-control" type="email" name="email" value="" id="email">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input class="form-control" type="password" name="password" value="" id="password">
+  </div>
+  <div class="form-group">
+    <label for="passwordcheck">Password check</label>
+    <input class="form-control" type="password" name="passwordcheck" value="" id="passwordcheck">
+  </div>
+<input type="submit" name="submitsignup" value="Submit" class="btn btn-primary">
 </form>
-<a href=".">login</a>
+<a href="." class="col-12">login</a>
+</div>
+</div>
 
 <?php
 include 'footer.php';
--- view/connection.php
+++ view/connection.php
@@ -1,12 +1,23 @@
 <?php
 include 'header.php';
  ?>
+ <div class="container d-flex justify-content-center">
+   <div class="row">
 <form class="" action="index.php" method="post">
-<input type="email" name="email" value="">
-<input type="password" name="password" value="">
-<input type="submit" name="submitlogin" value="Submit">
+  <div class="form-group">
+  <label for="email">Email</label>
+  <input class="form-control" type="email" name="email" value="" id="email">
+</div>
+<div class="form-group">
+  <label for="password">Password</label>
+  <input class="form-control" type="password" name="password" value="" id="password">
+</div>
+<input type="submit" name="submitlogin" value="Submit" class="btn btn-primary">
 </form>
-<a href="?signup">SignUp</a>
+<a href="?signup" class="col-12">SignUp</a>
+</div>
+</div>
+
 
 <?php
 include 'footer.php';
