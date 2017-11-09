22a0ec67493142266dc72c290c7d66bb40b391ec
benjamin <degroote.benjamin@gmail.com>
2017-11-09 10:15:09 +0100

    create view with all account

--- view/index.php
+++ view/index.php
@@ -1,8 +1,20 @@
 <?php
 include 'header.php';
+foreach ($list as $key => $value) {
 ?>
-
-
+<div class="container">
+<ul class="list-group col-5">
+  <li class="list-group-item active d-flex justify-content-center"><?php echo $value->getType() ?></li>
+  <li class="list-group-item">Amount : <?php echo $value->getAmount()?></li>
+  <li class="list-group-item d-flex justify-content-around">
+    <a class="btn btn-primary" href="?iddelete=<?php echo $value->getIdAccount();?>"><i class="fa fa-trash" aria-hidden="true"></i></a>
+    <a class="btn btn-primary" href="detail.php?iddetail=<?php echo $value->getIdAccount();?>"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
+  </li>
+</ul>
+</div>
+<?php
+}
+?>
 
 
 <?php
