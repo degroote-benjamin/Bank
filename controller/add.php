0fd3dbf7a3a5d5c0c7ad92884347f55f0b865924
benjamin <degroote.benjamin@gmail.com>
2017-11-09 15:06:16 +0100

    finish transfer with withdraw and add money , need to do security

--- controller/add.php
+++ controller/add.php
@@ -5,32 +5,46 @@ $managerUser = new Usermanager($db);
 $managerA = new Accountmanager($db);
 
 // if click on submitamount from detail.php
-if(isset($_POST['submitamount'])){
-  if(isset($_POST['amount'],$_POST['bank'])){
-    $account = $managerA->get($_POST['iddetail']);
-    if($_POST['bank'] == "withdrawal"){
-      $account->withdrawal($_POST['amount']);
+if (isset($_POST['submitamount'])) {
+    if (isset($_POST['amount'],$_POST['bank'])) {
+        $account = $managerA->get($_POST['iddetail']);
+        if ($_POST['bank'] == "withdrawal") {
+            $account->withdrawal($_POST['amount']);
+        } else {
+            $account->add($_POST['amount']);
+        }
+        $managerA->update($account);
     }
-    else {
-      $account->add($_POST['amount']);
-    }
-    $managerA->update($account);
-  }
 }
 
-if(isset($_GET['iddetail']) || isset($_POST['iddetail'])){
-  if(isset($_GET['iddetail'])){
-    $id = $_GET['iddetail'];
-  }
-  else {
-    $id = $_POST['iddetail'];
-  }
-  $account = $managerA->get($id);
-  include 'view/detail.php';
+if (isset($_POST['submittransfer'])) {
+    if (isset($_POST['credit'],$_POST['debit'],$_POST['amount'])) {
+        if (isset($_POST['credit'])) {
+            $credit = $managerA->get($_POST['credit']);
+            $credit->add($_POST['amount']);
+            $managerA->update($credit);
+        }
+        if (isset($_POST['debit'])) {
+            $debit = $managerA->get($_POST['debit']);
+            $debit->withdrawal($_POST['amount']);
+            $managerA->update($debit);
+        }
+    }
 }
-else if(isset($_GET['transfer'])){
-  include 'view/transfer.php';
-}
-else {
-include 'view/add_account.php';
+
+// if id detail exist
+if (isset($_GET['iddetail']) || isset($_POST['iddetail'])) {
+    // create universal $id
+    if (isset($_GET['iddetail'])) {
+        $id = $_GET['iddetail'];
+    } else {
+        $id = $_POST['iddetail'];
+    }
+    $account = $managerA->get($id);
+    include 'view/detail.php';
+} elseif (isset($_GET['transfer'])) {
+    $list=$managerA->getList($_SESSION['user']);
+    include 'view/transfer.php';
+} else {
+    include 'view/add_account.php';
 }
---
+++ view/transfer.php
@@ -0,0 +1,33 @@
+<?php
+include 'header.php';
+ ?>
+ <div class="container d-flex justify-content-center">
+   <div class="row">
+ <form action="add.php?transfer" method="post" class="my-5">
+ <div class="form-group">
+   <label for="bank">debit</label>
+   <select name="debit" class="form-control">
+     <?php foreach ($list as $key => $value){ ?>
+       <option value="<?php echo $value->getIdAccount()?>"><?php echo $value->getType() . "   " . $value->getAmount()?></option>
+     <?php } ?>
+   </select>
+ </div>
+ <div class="form-group">
+ <label for="bank">credit</label>
+ <select name="credit" class="form-control">
+   <?php foreach ($list as $key => $value){ ?>
+     <option value="<?php echo $value->getIdAccount()?>"><?php echo $value->getType() . "   " . $value->getAmount()?></option>
+   <?php } ?>
+ </select>
+</div>
+ <div class="form-group">
+   <label for="amount">Amount</label>
+   <input class="form-control" type="text" name="amount" value="" id="amount">
+ </div>
+ <input type="submit" name="submittransfer" value="Submit" class="btn btn-primary">
+ </form>
+ </div>
+ </div>
+ <?php
+include "footer.php";
+  ?>
